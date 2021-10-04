618ab564aa26aa654031ef5130201a805536b353
Storm Morzos <storm.morzos@gmail.com>
2021-10-03 19:13:33 -0600

    turn in

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
-const api = require('./routes/index.js');
+const uuid = require('./helpers/uuid');
+const DB = require('./db/db.json');
 
 const PORT = 3001;
 
@@ -8,11 +10,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 
 app.use(express.static('public'));
 
-
+// GET
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
@@ -20,6 +21,47 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get("/api/notes", (req, res) => {
+  res.status(200).json(DB);
+});
+
+// Post
+app.post('/api/notes', (req ,res) =>{
+  console.info(`${req.method} request received to add a note`);
+  const { title, text } = req.body;
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      review_id: uuid(),
+    };
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+    console.log(response);
+    res.status(201).json(response);
+
+  //adding in note
+    let stringy = JSON.stringify(newNote);
+    let curentNotesGet = fs.readFileSync('./db/db.json', 'utf-8');
+    let curentNotes = curentNotesGet.substring(1, curentNotesGet.length-1);
+    //console.log(stringy);
+    //console.log(curentNotes);
+    //console.log("--------------------");
+    let newNotes ="["+curentNotes+","+stringy+"]";
+    //console.log(newNotes);
+    fs.writeFile('./db/db.json',newNotes,(err)=>{
+      if(err){
+         console.log(err);
+      }
+    });
+    window.location.reload();
+  } else {
+    res.status(500).json('Error in posting Note');
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
